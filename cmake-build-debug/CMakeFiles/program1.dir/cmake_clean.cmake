f8a44a84778dd669548ef3145fbdb897c20afbc8
Micklerick617 <mlennon@smu.edu>
2022-02-22 01:05:45 -0600

    Program 1

---
+++ cmake-build-debug/CMakeFiles/program1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program1.dir/main.c.obj"
+  "libprogram1.dll.a"
+  "program1.exe"
+  "program1.exe.manifest"
+  "program1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/program1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
